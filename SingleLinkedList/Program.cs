728290511ff95edb8ba508932033ccda3f761ed7
rahulanasane <rahul_anasane@hotmail.com>
2021-04-03 12:15:34 -0400

    singleLinkedList - BasicOperations

--- SingleLinkedList/Program.cs
+++ SingleLinkedList/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 
-namespace SingleLinkedList
+namespace SingleLinedkList
 {
+    class Node
+    {
+        private int data;
+        private Node next;
+
+        public Node()
+        {
+            data = 0;
+            next = null;
+        }
+        public Node(int value)
+        {
+            data = value;
+            next = null;
+        }
+        public Node InsertNext(int value)
+        {
+
+            Node node = new Node(value);
+            if (this.next == null)
+            {
+                node.next = null;
+                this.next = node;
+            }
+            else
+            {
+                Node temp = this.next;
+                node.next = temp;
+                this.next = node;
+            }
+            return node;
+        }
+        public int DeleteNext()
+        {
+            if (next == null)
+                return 0;
+            Node node = this.next;
+            this.next = this.next.next;
+            node = null;
+            return 1;
+        }
+        public void Traverse(Node node)
+        {
+            if (node == null)
+                node = this;
+
+            System.Console.WriteLine("Traversing Singly Linked List :");
+
+            while (node != null)
+            {
+                System.Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node node1 = new Node(100);
+            Node node2 = node1.InsertNext(200);
+            Node node3 = node2.InsertNext(300);
+            Node node4 = node3.InsertNext(400);
+            Node node5 = node4.InsertNext(500);
+            node1.Traverse(null);
+            Console.WriteLine("Deleting from Linked List...");
+            node3.DeleteNext();
+            node2.Traverse(null);
+            System.Console.ReadLine();
         }
     }
 }
